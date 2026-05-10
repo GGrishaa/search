98527e7ae28d6222c4a735d1fbdc5fbd45692f6c
Григорий <gamironov@edu.hse.ru>
2026-05-10 03:02:14 +0300

    dox tree + lin_search

--- search.cpp
+++ search.cpp
@@ -4,6 +4,23 @@
 #include "tree.hpp"
 using namespace std;
 
+/**
+ * @file search.cpp
+ * @brief Реализация линейного поиска, функций отсчета затраченного времени
+ */
+
+/**
+ * @brief Функция линейного поиска
+ *
+ * Последовательно проходит по всем элементам и добавляет их в случае
+ * соответствия ключу
+ * @param flats указатель на начало массива для поиска
+ * @param size размер массива в котором будет проходить поиск
+ * @param find указатель на начало массива найденных элементов
+ * @param key ключ-строка, по которой будет проходить поиск
+ * @return количество найденных элементов
+ */
+
 size_t linear_search(flat* flats, size_t size, flat* find, string key) {
   if (!flats) return 0;
   size_t ans = 0;
@@ -16,6 +33,9 @@ size_t linear_search(flat* flats, size_t size, flat* find, string key) {
   return ans;
 }
 
+/**
+ * @brief Точка начала работы программы
+ */
 int main() {
   size_t s;
   flat* flats = load_from_csv("CSV/apartments_100.csv", s);
